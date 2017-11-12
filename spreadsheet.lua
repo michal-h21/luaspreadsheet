eae8b848d43f7b4b24b1aca9c78cda852bd5a5fd
Michal Hoftich <michal.h21@gmail.com>
2017-11-12 21:35:25 +0100

    Load sample xlsx file and try to parse it

--- spreadsheet.lua
+++ spreadsheet.lua
@@ -1,5 +1,11 @@
+kpse.set_program_name "luatex"
 local xslt = require "spreadsheet.xslt-reader"
 
-local obj = xslt.load("pokus.xslt")
+local obj,msg  = xslt.load("pokus.xlsx")
+
+if not obj then
+  print(msg)
+  os.exit()
+end
 
 
