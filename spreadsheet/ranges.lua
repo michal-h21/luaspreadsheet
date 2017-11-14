c4348fdc08d4d54e86a6b79bce9c3746457fa5a0
Michal Hoftich <michal.h21@gmail.com>
2017-11-14 17:37:39 +0100

    Added some comments

--- spreadsheet/ranges.lua
+++ spreadsheet/ranges.lua
@@ -1,12 +1,15 @@
 -- module for range handling
 local m = {}
 
+--- Parse ranges in the form A1:B2 to the x, y, x1, y1 form
 local function get_range(range)
   local range = range:lower()
+  -- convert the letter to number
   local function getNumber(s)
     if s == "" or s == nil then return nil end
     local f,ex = 0,0
     for i in string.gmatch(s:reverse(),"(.)") do
+      -- calculate the number corresponding to the letter
       f = f + (i:byte()-96) * 26 ^ ex
       ex = ex + 1
     end
