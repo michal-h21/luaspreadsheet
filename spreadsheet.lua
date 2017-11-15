0ebbad41942cbab1bf7af950e8ef5b716d62e37a
Michal Hoftich <michal.h21@gmail.com>
2017-11-15 16:00:12 +0100

    Use only smaller test file

--- spreadsheet.lua
+++ spreadsheet.lua
@@ -3,15 +3,15 @@ local xslt = require "spreadsheet.xslt-reader"
 
 local lo,msg  = xslt.load("pokus.xlsx")
 
-local obj,msg  = xslt.load("odpis.xlsx")
+-- local obj,msg  = xslt.load("odpis.xlsx")
 
-if not obj then
+if not obj and not lo then
   print(msg)
   os.exit()
 end
 
 local sheet = lo:get_sheet("Sheet1")
-local sheet = obj:get_sheet("Sheet1")
+-- local sheet = obj:get_sheet("Sheet1")
 -- local sheet = obj:get_sheet("List1")
-local sheet = obj:get_sheet(1)
+-- local sheet = obj:get_sheet(1)
 
