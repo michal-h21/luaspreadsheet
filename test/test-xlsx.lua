4a685e43b9384de30332455ae3904b09f6d01bc5
Michal Hoftich <michal.h21@gmail.com>
2017-11-20 10:14:10 +0100

    Basic xlsx testing

--- test/test-xlsx.lua
+++ test/test-xlsx.lua
@@ -1,18 +1,18 @@
-require "busted.runner"
+require "busted.runner" ()
 kpse.set_program_name "luatex"
 
 local log = require "spreadsheet.log"
 local xlsx = require "spreadsheet.xlsx-reader"
 
 log.level="warn"
-
-local lo,msg  = xlsx.load("pokus.xlsx")
-
--- local obj,msg  = xslt.load("odpis.xlsx")
-
-if not obj and not lo then
-  print(msg)
-  os.exit()
-end
-
-local sheet = lo:get_sheet("Sheet1")
+describe("Basic xlsx file loading should work", function()
+  local lo,msg  = xlsx.load("test/pokus.xlsx")
+  it("Parse the xlst file", function()
+    assert.truthy(lo)
+  end)
+  local sheet = lo:get_sheet("Sheet1")
+  it("Load a sheet", function()
+    assert.truthy(sheet)
+    assert.same(type(sheet), "table")
+  end)
+end)
